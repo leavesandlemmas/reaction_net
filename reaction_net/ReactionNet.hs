a2337d57586ac5b6098bf67c6559e1fd7357178a
Scott Oswald <soswald77@gmail.com>
2025-07-22 16:53:15 -0500

    not working parser

--- reaction_net/ReactionNet.hs
+++ reaction_net/ReactionNet.hs
@@ -1,3 +1,21 @@
 module Main (main) where 
 
-main = printLn "Hello World!"
+
+import Text.Parsec
+
+main = putStrLn "Hello World!"
+
+-- adapted from https://hasura.io/blog/parser-combinators-walkthrough
+-- newtype Parser a = Parser {
+--     runParser :: String -> (String, Either ParseError a)
+-- }
+
+-- any :: Parser Char 
+-- any = Parser $ \input -> case input of 
+--     (x:xs) -> (xs, Right x)
+--     [] -> ("", Left $ ParseError "any character" "the end of the input")
+
+-- eof :: Parser ()
+-- eof = Parser $ \input -> case input of 
+--     [] -> ("", Right ())
+--     (c:_) -> (input, Left $ ParseError "the end of the input" [c] )
