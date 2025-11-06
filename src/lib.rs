f234ddf98b21d7addd6d7b1c23b089a0a5f8a605
leavesandlemmas <60299100+leavesandlemmas@users.noreply.github.com>
2025-11-06 09:12:41 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -51,35 +51,12 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         println!("{USAGE}");
         return Ok(());
     }
-    let mut tokens: Vec<Symbol> = Vec::new();
 
     for file in config.files {
         let contents = fs::read_to_string(file)?;
-        let mut content_iterator =  contents.chars().peekable();
-        while let Some(c) = content_iterator.next() {
-            match c {
-                ' ' => continue,
-                '\r' => continue,
-                '\t' => continue,
-                '\n' => continue,
-                '(' => tokens.push(Symbol::LeftParen),
-                ')' => tokens.push(Symbol::RightParen),
-                '+' => tokens.push(Symbol::Plus),
-                '*' => tokens.push(Symbol::Star),
-                ';' => tokens.push(Symbol::SemiColon),
-                ':' => tokens.push(Symbol::Colon),
-                '=' => tokens.push(Symbol::Equal),
-                '-' => {
-                    if let Some('>') = content_iterator.peek() {
-                        content_iterator.next();
-                        tokens.push(Symbol::RightArrow);
-                    } else {
-                        tokens.push(Symbol::Minus);
-                    }
-                }
-                _ => todo!(),
-            };
-        }
+        let tokens = Tokens::parse(contents.chars())?;
+
+        tokens.print();
     }
 
     Ok(())
@@ -100,10 +77,14 @@ Options:
      ";
 
 // symbols
-
+#[derive(Debug)]
 enum Symbol {
     LeftParen,
     RightParen,
+    LeftBrace,
+    RightBrace,
+    LeftBracket,
+    RightBracket,
     Plus,
     Star,
     SemiColon,
@@ -116,6 +97,9 @@ enum Symbol {
     RightArrow,
     LeftArrow,
     LeftRightArrow,
+    Tick,
+    Quote,
+    Comma,
     Species(String),
     Number(u32),
 }
@@ -124,3 +108,102 @@ struct Tokens {
     tokens: Vec<Symbol>,
 }
 
+impl Tokens {
+    fn parse(characters: impl Iterator<Item = char>) -> Result<Tokens, String> {
+        let mut tokens: Vec<Symbol> = Vec::new();
+        let mut iterator = characters.peekable();
+        let mut line: u64 = 1;
+        while let Some(c) = iterator.next() {
+            if c.is_whitespace() {
+                if c == '\n' {
+                    line += 1
+                }
+            } else {
+                match c {
+                    '(' => tokens.push(Symbol::LeftParen),
+                    ')' => tokens.push(Symbol::RightParen),
+                    '{' => tokens.push(Symbol::LeftBrace),
+                    '}' => tokens.push(Symbol::RightBrace),
+                    '[' => tokens.push(Symbol::LeftBracket),
+                    ']' => tokens.push(Symbol::RightBracket),
+                    '+' => tokens.push(Symbol::Plus),
+                    '*' => tokens.push(Symbol::Star),
+                    ';' => tokens.push(Symbol::SemiColon),
+                    ':' => tokens.push(Symbol::Colon),
+                    '=' => tokens.push(Symbol::Equal),
+                    '"' => tokens.push(Symbol::Quote),
+                    '\'' => tokens.push(Symbol::Tick),
+                    ',' => tokens.push(Symbol::Comma),     
+                    '/' => {
+                        if let Some('/') = iterator.peek() {
+                            iterator.next();
+                            while let Some(c) = iterator.next() {
+                                if c == '\n' {
+                                    line += 1;
+                                    break ;
+                                }
+                            }
+                        } else if let Some('*') = iterator.peek(){
+                            iterator.next();
+                            while let Some(c) = iterator.next() {
+                                if c == '*' {
+                                    if  let Some('/') = iterator.peek() {break;}
+
+                                }
+                            }
+                        } else {tokens.push(Symbol::Slash);}
+                    }               
+                    '-' => {
+                        if let Some('>') = iterator.peek() {
+                            iterator.next();
+                            tokens.push(Symbol::RightArrow);
+                        } else {
+                            tokens.push(Symbol::Minus);
+                        }
+                    }
+                    '>' => tokens.push(Symbol::Greater),
+                    '<' => {
+                        if let Some('-') = iterator.peek() {
+                            iterator.next();
+                            if let Some('>') = iterator.peek() {
+                                tokens.push(Symbol::LeftRightArrow);
+                            } else {
+                                tokens.push(Symbol::LeftArrow);
+                            }
+                        } else {
+                            tokens.push(Symbol::Less);
+                        }
+                    }
+                    _ => {
+                        if c.is_alphanumeric() {
+                            let mut lexeme = String::new();
+                            lexeme.push(c);
+                            let mut number = c.is_ascii_digit();
+                            while let Some(c) = iterator.next() {
+                                if c.is_alphanumeric() {
+                                    number &= c.is_ascii_digit();
+                                    lexeme.push(c);
+                                } else {
+                                    break;
+                                }
+                            }
+                            if number {let n :u32= lexeme.parse().expect("Couldn't parse integer."); tokens.push(Symbol::Number(n));} else {
+tokens.push(Symbol::Species(lexeme));
+                            }
+                            
+                        } else {
+                            return Err(format!("Line {line}: Character not recognized {c}."));
+                        }
+                    }
+                };
+            }
+        }
+        Ok(Tokens { tokens })
+    }
+
+    fn print(&self) {
+        for token in &self.tokens {
+            println!("{token:?}");
+        }
+    }
+}
